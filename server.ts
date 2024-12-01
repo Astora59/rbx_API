c4ca7cbcf67301b15be44f65b4a0e92dca0cb831
Yamin Moulay <yaminmoulay12@gmail.com>
2024-12-01 15:59:25 +0100

    setting up dependencies

--- server.ts
+++ server.ts
@@ -2,3 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
